14dbd2a32c040a242e641f4175e80d8f49f86c38
irahudgin <irahudgin@gmail.com>
2023-10-15 19:06:47 -0400

    Finished the rest of the UI controls

---
+++ communitrade2/communiTrade/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using communiTrade.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace communiTrade.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewComand{ get; set; }
+        public RelayCommand DiscoveryViewCommand{ get; set; }
+
+        public HomeViewModel HomeVm{ get; set; }
+        public DiscoveryViewModel DiscoveryVM{ get; set; }
+
+        private object _currentView;
+
+		public object CurrentView
+		{
+			get { return _currentView; }
+			set { 
+					_currentView = value;
+					OnPropertyChanged();
+				}
+		}
+
+		public MainViewModel() 
+		{
+			HomeVm = new HomeViewModel();
+			DiscoveryVM = new DiscoveryViewModel();
+			CurrentView = HomeVm;
+
+			HomeViewComand = new RelayCommand(obj => 
+			{
+				CurrentView = HomeVm;
+			});
+
+            DiscoveryViewCommand = new RelayCommand(obj =>
+            {
+                CurrentView = DiscoveryVM;
+            });
+        }
+    }
+}
