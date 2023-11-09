b31420209e382f71c5e2cd05c9710ea728b6e74e
irahudgin <irahudgin@gmail.com>
2023-11-09 14:57:36 -0500

    updated featured items

--- communitrade2/communiTrade/MVVM/ViewModel/FeaturedViewModel.cs
+++ communitrade2/communiTrade/MVVM/ViewModel/FeaturedViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using communiTrade.Core;
 using communiTrade.MVVM.Model;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,13 +14,35 @@ using System.Windows.Markup;
 
 namespace communiTrade.MVVM.ViewModel
 {  
-    internal class FeaturedViewModel
+    internal class FeaturedViewModel : ObservableObject
     {
-        public async Task LoadFeaturedItemsAsync()
+        HttpClient client = new HttpClient();
+        private List<Item> _featuredItems;
+
+        public List<Item> FeaturedItems
         {
-            // Fetch data from the API and update FeaturedItems
-            // Example:
-            // FeaturedItems = await SomeApiService.GetFeaturedItemsAsync();
+            get {  return _featuredItems; }
+            set
+            {
+                _featuredItems = value;
+                OnPropertyChanged(nameof(FeaturedItems));
+            }
+        }
+
+        public FeaturedViewModel()
+        {
+            InitializeAsync();
+        }
+        private async void InitializeAsync()
+        {
+            client.BaseAddress = new Uri("https://localhost:7009/Trade/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            var server_response = await client.GetStringAsync("GetAllItems");
+            Response response_JSON = JsonConvert.DeserializeObject<Response>(server_response);
+
+            FeaturedItems = response_JSON.items;
         }
     }
         
